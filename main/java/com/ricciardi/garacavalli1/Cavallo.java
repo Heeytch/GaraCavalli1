e59bc19206e93a6dae1d9e493df14a03b3a633e8
Heeytch <160403469+Heeytch@users.noreply.github.com>
2024-11-07 19:39:27 +0100

    Add files via upload

---
+++ main/java/com/ricciardi/garacavalli1/Cavallo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ricciardi.garacavalli1;
+
+/**
+ *
+ * @author highm
+ */
+public class Cavallo {
+    
+}
